68aa137aeb38ba8de870d1e9361e4b135d4a3a41
Yana Belinskaya <belikbelka@icloud.com>
2024-05-13 15:22:50 +0300

    Pr6

---
+++ pr6/View/Calendar.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace pr6
+{
+    public partial class Calendar : UserControl
+    {
+        public string Day { get; set; } = "5";
+        public Calendar()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Удалить все дочерние элементы из родительского контейнера
+            (Parent as Panel)?.Children.Clear();
+            // Открыть страницу Vibor
+            MainWindow mainWindow = Application.Current.MainWindow as MainWindow;
+            if (mainWindow != null)
+            {
+                mainWindow.PageFrame.Navigate(new Uri("View/Vibor.xaml", UriKind.Relative));
+            }
+        }
+
+    }
+}
---
+++ pr6/View/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace pr6
+{
+    public partial class MainWindow : Window
+    {
+        DateTime dateTime = DateTime.Now;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Generate();
+        }
+        private void Button_Click1(object sender, RoutedEventArgs e)
+        {
+            if (PageFrame.Content != null && PageFrame.Content.GetType() == typeof(Vibor))
+            {
+                PageFrame.Content = null;
+            }
+            dateTime = dateTime.AddMonths(-1);
+            Generate();
+            
+        }
+        private void Button_Click2(object sender, RoutedEventArgs e)
+        {
+            dateTime = dateTime.AddMonths(1);
+            Generate();
+        }
+
+        void Generate()
+        {
+            daytxt.Text = dateTime.ToString("MMMM yyyy г.");
+            calendarik.Children.Clear();
+            for (int i = 1; i <= DateTime.DaysInMonth(dateTime.Year, dateTime.Month); i++)
+            {
+                Calendar calendar = new Calendar();
+                calendar.Day = i.ToString();
+                calendarik.Children.Add(calendar);
+            }
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void PageFrame_Navigated(object sender, NavigationEventArgs e)
+        {
+            if (PageFrame.Content != null && PageFrame.Content.GetType() == typeof(Vibor))
+            {
+                // Скрыть кнопку "Сюда"
+                Suda.Visibility = Visibility.Collapsed;
+
+                // Показать кнопку "Сохранить"
+                Save.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                // Показать кнопку "Сюда"
+                Suda.Visibility = Visibility.Visible;
+
+                // Скрыть кнопку "Сохранить"
+                Save.Visibility = Visibility.Collapsed;
+            }
+        }
+    }
+}
